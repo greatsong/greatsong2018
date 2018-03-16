be095139b9f31b270ffbf55246ec5614c28411d1
송석리(Song Sukree) <greatsong21@gmail.com>
2018-03-16 16:45:07 +0900

    3주차 데이터 및 파일 업로드

---
+++ 3_eng_quiz.py
@@ -0,0 +1,33 @@
+﻿import random
+f = open('eng.txt')
+quiz = open('quiz.txt','w')
+cnt = 0
+ans = []
+for row in f :
+    if cnt % 2 == 0 :
+        row = row.split()
+        no = random.randrange(len(row))
+        ans.append(row[no])
+        row[no] = '[     ]'
+    cnt += 1
+
+    for i in row :
+        quiz.write(i + ' ')
+    quiz.write('\n')
+
+f.close()
+quiz.close()
+
+r = open('quiz.txt')
+print(r.read())
+r.close()
+
+random.shuffle(ans)
+print(ans)
+
+f = open('quiz.txt','a')
+f.write('\n[보기]\n')
+for i in ans :
+    msg = i + ' '
+    f.write(msg)
+f.close()
